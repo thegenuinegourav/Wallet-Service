a491ff76f0d73ee55f8fc24d8da9142b8c3967c6
Gourav Suri <gourav.suri@grabtaxi.com>
2021-04-13 21:20:21 +0530

    init unit tests

--- controller/user-controller_test.go
+++ controller/user-controller_test.go
@@ -1,1 +1,33 @@
 package controller
+
+import (
+	"encoding/json"
+	"github.com/WalletService/mocks"
+	"github.com/WalletService/model"
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetUser(t *testing.T) {
+	userService := new(mocks.IUserService)
+	expectedResult := model.User{
+		Name: "test_user",
+		Email: "test_email",
+		Mobile: "test_mobile",
+	}
+	userService.On("GetUserService", 1).Return(&expectedResult, nil)
+	userController := NewUserController(userService)
+
+	req := httptest.NewRequest("GET", "http://localhost:8080/api/v1/user/1", nil)
+	w := httptest.NewRecorder()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/api/v1/user/{id}", userController.GetUser)
+	r.ServeHTTP(w, req)
+
+	actualResult := model.User{}
+	json.NewDecoder(w.Body).Decode(&actualResult)
+	assert.Equal(t, expectedResult,actualResult)
+}
--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/gorm v1.9.16
 	github.com/robfig/cron v1.2.0
+	github.com/stretchr/testify v1.7.0
 )
